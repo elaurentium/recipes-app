a3d0e11a92dd78ed922c7ba3a5f94c0f73c7388f
AndersonCR <anderson.c.rodrigues@hotmail.com>
2023-01-26 11:29:13 -0300

    [fix] corrige erros de merge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Switch } from 'react-router-dom';
-import Test from './tests/Test';
-
-function App() {
-  return (
-    <Switch>
-      <Route path="/meals" component={ Test } exact />
-      <Route path="/drinks" component={ Test } exact />
-    </Switch>
-=======
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
@@ -20,13 +6,12 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-  <div className="meals">
+    <div className="meals">
       <Switch>
         <Route exact path="/" component={ Login } />
       </Switch>
       <Footer />
     </div>
->>>>>>> edde7bfceeecac680a09b7da2d1112014750f954
   );
 }
 
