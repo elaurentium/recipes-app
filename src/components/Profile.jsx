a8d799da610b587bd6dd92e962fddeed5fb2f361
Seu nome <marcusaviotti@gmail.com>
2023-01-26 17:07:36 -0300

    feat Adiciona redirecionamento para a rota de Done Recipes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Profile() {
   const getEmail = localStorage.getItem('user');
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/done-recipes');
+  };
 
   return (
     <div>
@@ -22,6 +29,7 @@ export default function Profile() {
           data-testid="profile-done-btn"
           name="rofile-done-btn"
           id="rofile-done-btn"
+          onClick={ handleClick }
         >
           Done Recipes
         </button>
